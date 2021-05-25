b6b6d7c9974aa866317efee88081738a66568467
Qiufen-chen <qfchen@213>
2021-05-25 16:56:25 +0800

    tset

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/25 16:26
+# @Author  : Qiufen.Chen
+# @Email   : 1760812842@qq.com
+# @File    : test.py
+# @Software: PyCharm
+
+
+print('this is a test script!')
+
+print('Test successfully')
